21a28e3849328c51ccf8429d3d2304e64c299cb8
SuhasReddy-S <71500442+SuhasReddy-S@users.noreply.github.com>
2020-10-23 19:14:03 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		this.image = loadImage("sprites/paper.png");
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:0.4		
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+	}
+	display()
+	{
+    	var paperpos=this.body.position;		
+			push()
+			translate(paperpos.x, paperpos.y);
+			image(this.image,0,0,this.r,this.r);
+			pop()	
+			drawSprites();	
+	}
+}
